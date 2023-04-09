489b8111058264f98794cde14dd4d7f17315660e
Coding Boy <chaiyuanyuan432@gmail.com>
2023-04-09 18:25:33 +0800

    test branch

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from "./redux";
 import './index.css'
-
+const a = '';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
