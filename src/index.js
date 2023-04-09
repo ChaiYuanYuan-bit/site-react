bccd8d5f8661e66ee666482a396e3933c32e0da5
bzh <18361576691@163.com>
2023-04-09 18:30:44 +0800

    test branch

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import store from "./redux";
 import './index.css'
 const a = '';
+const b =''
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
