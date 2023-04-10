8f327b07d7dfed88c80174b247885e75c7effc73
Coding Boy <chaiyuanyuan432@gmail.com>
2023-04-10 13:15:44 +0800

    comerge with bzh

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,20 @@
 import Login from "../views/Login";
 import Register from "../views/Register";
 import Layout from "../views/Layout";
+import Mall from "../views/Mall";
 import NotFound from '../views/NotFound'
 
 //创建路由表
 const routes = (props)=>[
     {
         path:'/home',
-        element:<Layout {...props}/>
+        element:<Layout {...props}/>,
+        children:[
+            {
+                path:'mall',
+		        element:<Mall/>,
+            },
+        ]
     },
     {
         path:'/register',
@@ -15,8 +22,8 @@ const routes = (props)=>[
     },
     {
         path:'/',
-        // element:<Login {...props}/>
-        element:<Layout {...props}/>
+        element:<Login {...props}/>
+        // element:<Layout {...props}/>
     },
     {
         path:'*',
--- src/views/Layout/index.js
+++ src/views/Layout/index.js
@@ -1,62 +1,24 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import {
     MenuFoldOutlined,
     MenuUnfoldOutlined,
     UploadOutlined,
     UserOutlined,
-    HomeOutlined,
     VideoCameraOutlined,
-    NotificationOutlined,
-    MailOutlined
   } from '@ant-design/icons';
-import { Layout, Menu, theme } from 'antd';
-import { useNavigate } from 'react-router-dom';
+import { Layout, Menu, theme} from 'antd';
+import { useNavigate,Outlet } from 'react-router-dom';
 import './Layout.scss'
 const { Header, Sider, Content } = Layout;
 
-//顶部菜单
-const headerItems = [
-    {
-        label: '个人中心',
-        key: 'mine',
-        icon: <UserOutlined />,
-        children: [
-          {
-            key:'myInfo',
-            label: '个人信息',
-          },
-          {
-            key:'setPwd',
-            label: '修改密码',
-          },
-          {
-              key:'exit',
-              label: '退出系统',
-            },
-        ],
-      },
-    {
-    label: '通知',
-    key: 'note',
-    icon: <NotificationOutlined />,
-    },
-    {
-        label: '邮件',
-        key: 'mail',
-        icon: <MailOutlined />,
-    },
-    {
-        label: '首页',
-        key: 'home',
-        icon: <HomeOutlined />,
-    },
-];
-
 export default () => {
-    //跳转路由
+    // 跳转路由
     const navigate = useNavigate();
-    //顶部菜单蓝状态
-    const [current, setCurrent] = useState('home');
+    // 配置默认路由路径
+    const [route,setRoute] = useState('/home/mall')
+    useEffect(()=>{
+
+    },[])
     //退出
     const handleExit = ()=>{
         //清除系统缓存
@@ -65,74 +27,74 @@ export default () => {
         //跳转到登录页
         navigate('/')
     }
-
     const [collapsed, setCollapsed] = useState(false);
-
-    //顶部菜单点击事件
-    const handleTop = (e) => {
-        setCurrent(e.key);
-      };
 
     const {
         token: { colorBgContainer },
     } = theme.useToken();
 
     return (
-        <Layout className='ant-layout'>
-      <Sider trigger={null}
-       collapsible 
-       collapsed={collapsed} 
-       height={'100%'}
-       >
-        <div className="logo" ></div>
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1',
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
-            },
-          ]}
-        />
-      </Sider>
-      <Layout>
-        <Header
-          style={{
-            padding: 0,
-            background: colorBgContainer,
-            display:'flex'
-          }}
-        >
-          {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-            className: 'trigger',
-            onClick: () => setCollapsed(!collapsed),
-          })}
-         <Menu style={{flexDirection:'row-reverse',flex:1}} onClick={handleTop} selectedKeys={[current]} mode="horizontal" items={headerItems} />
-        </Header>
-        <Content
-          style={{
-            margin: '24px 16px',
-            padding: 24,
-            minHeight: 280,
-            background: colorBgContainer,
-          }}
-        >
-          Content
-        </Content>
-      </Layout>
-    </Layout>
+        <div>
+          <button onClick={handleExit}>退出</button>
+          <Sider trigger={null}
+          collapsible 
+          collapsed={collapsed} 
+          height={'100%'}
+          >
+            <div className="logo" ></div>
+            <Menu
+              theme="dark"
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              items={[
+                {
+                  key: '1',
+                  icon: <UserOutlined />,
+                  label: 'nav 1',
+                },
+                {
+                  key: '2',
+                  icon: <VideoCameraOutlined />,
+                  label: 'nav 2',
+                },
+                {
+                  key: '3',
+                  icon: <UploadOutlined />,
+                  label: 'nav 3',
+                },
+              ]}
+            />
+          </Sider>
+          <Layout
+            style={{
+              background: '#eee',
+              display:'flex'
+            }}
+          >
+            <Header
+              style={{
+                padding: 0,
+                background: colorBgContainer,
+                display:'flex'
+              }}
+            >
+              {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                className: 'trigger',
+                onClick: () => setCollapsed(!collapsed),
+              })}
+            </Header>
+         
+            <Content
+              style={{
+                margin: '24px 16px',
+                padding: 24,
+                minHeight: 280,
+                background: colorBgContainer,
+              }}
+            >
+              <Outlet/>
+            </Content>
+          </Layout>
+        </div>
     );
 }
