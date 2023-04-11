f22988eadb083e71ef6c3e4d902fb408cc7b1dd0
bzh <18361576691@163.com>
2023-04-11 09:36:29 +0800

    update layout sider/header

--- src/views/Layout/index.js
+++ src/views/Layout/index.js
@@ -2,11 +2,14 @@ import React,{useState,useEffect} from 'react';
 import {
     MenuFoldOutlined,
     MenuUnfoldOutlined,
-    UploadOutlined,
     UserOutlined,
-    VideoCameraOutlined,
+    PoweroffOutlined,
+    FileTextOutlined,
+    UsergroupAddOutlined,
+    ToolOutlined,
+    FileDoneOutlined
   } from '@ant-design/icons';
-import { Layout, Menu, theme} from 'antd';
+import { Layout, Menu, theme,Button,Carousel} from 'antd';
 import { useNavigate,Outlet } from 'react-router-dom';
 import './Layout.scss'
 const { Header, Sider, Content } = Layout;
@@ -32,56 +35,65 @@ export default () => {
     const {
         token: { colorBgContainer },
     } = theme.useToken();
+    
+    function getItem(label, key, icon, children, type) {
+      return {
+        key,
+        icon,
+        children,
+        label,
+        type,
+      };
+    }
+
 
     return (
-        <Layout>
-          {/* <button onClick={handleExit}>退出</button> */}
+      <Layout>
           <Sider trigger={null}
-          collapsible 
-          collapsed={collapsed} 
-          height={'100%'}
-          >
-            <div className="logo" ></div>
-            <Menu
-              theme="dark"
-              mode="inline"
-              defaultSelectedKeys={['1']}
-              items={[
-                {
-                  key: '1',
-                  icon: <UserOutlined />,
-                  label: 'nav 1',
-                },
-                {
-                  key: '2',
-                  icon: <VideoCameraOutlined />,
-                  label: 'nav 2',
-                },
-                {
-                  key: '3',
-                  icon: <UploadOutlined />,
-                  label: 'nav 3',
-                },
-              ]}
-            />
+            collapsible 
+            collapsed={collapsed} 
+            height={'100%'}
+            >
+              <div className="logo" ><p>EIS系统</p></div>
+              <Menu
+                theme="dark"
+                mode="inline"
+                defaultSelectedKeys={['1']}
+                items={[
+                  getItem('我的', 'sub1', <UserOutlined />, [
+                    getItem('订单信息', '1',<FileTextOutlined />),
+                  ]),
+                  getItem('后台管理', 'sub2', <ToolOutlined />, [
+                    getItem('订单管理', '2',<FileDoneOutlined />),
+                    getItem('用户管理', '3',<UsergroupAddOutlined />),
+                  ]),
+                ]}
+              />
+
           </Sider>
-          <Layout
-            style={{
-              background: '#eee',
-              display:'flex'
-            }}
-          >
+
+          <Layout className="site-layout" >
             <Header
-              style={{
+               style={{
                 padding: 0,
-                background: colorBgContainer,
-                display:'flex'
-              }}
+                background: colorBgContainer, 
+               }}
             >
               {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
                 className: 'trigger',
                 onClick: () => setCollapsed(!collapsed),
               })}
+              {/* <div className='video'>
+                <video src="http://localhost:3000/island.mp4" autoplay muted loop></video>
+              </div> */}
+              <Button
+                className='exit'
+                onClick={handleExit}
+                type="primary"
+                icon={<PoweroffOutlined />}
+              >
+                退出登录
+              </Button>
             </Header>
          
             <Content
@@ -95,6 +107,6 @@ export default () => {
               <Outlet/>
             </Content>
           </Layout>
-        </Layout>
+      </Layout>
     );
 }
