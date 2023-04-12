d345b5cbb7b79ebd8caf6db4ae2a34eaf9f8891f
bzh <18361576691@163.com>
2023-04-12 17:06:45 +0800

    change hotelname

--- src/components/CardGroup/index.jsx
+++ src/components/CardGroup/index.jsx
@@ -21,7 +21,7 @@ const CardGroup = ({allGoods,handleCardClick}) => {
                         }}
                         cover={<img className='mycard-image' alt="example" src={item.detail?item.detail.imageUrl:'网络错误'} />}
                     >
-                    <Meta title={item.detail?item.detail.hotelName.trim():'网络错误'} description={item.detail?item.detail.description.trim():'网络错误'} />
+                    <Meta title={item.detail?item.detail.name.trim():'网络错误'} description={item.detail?item.detail.description.trim():'网络错误'} />
                     </Card>
                     ))
                 }
