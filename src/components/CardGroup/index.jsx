8ddd92611bb94e9ad263966a05960cf4a0c9472e
Coding Boy <chaiyuanyuan432@gmail.com>
2023-04-13 09:38:15 +0800

    update markdown components flash

--- src/components/CardGroup/index.jsx
+++ src/components/CardGroup/index.jsx
@@ -22,7 +22,7 @@ const CardGroup = ({allGoods,handleCardClick}) => {
                         }}
                         cover={<img className='mycard-image' alt="example" src={item.detail?item.detail.imageUrl:'网络错误'} />}
                     >
-                    <Meta title={item.detail?item.detail.name.trim():'网络错误'} description={item.detail?item.detail.description.trim():'网络错误'} />
+                    <Meta  className='mycard-meta' title={item.detail?item.detail.name.trim():'网络错误'} description={item.detail?item.detail.description.trim():'网络错误'} />
                     </Card>
                     ))
                 }
